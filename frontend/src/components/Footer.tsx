29422a44d6a483d3f6cda9c9d48d857499abd543
nazifa <nazifabari@gmail.com>
2026-07-28 21:56:12 -0400

    fixed responsive design bugs for mobile

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer(){
 
 return(
 
-    <footer className="flex items-center justify-between px-10 md:px-40 py-13 border bg-[#EBE1D0] border-[#EBE2E9] ">
+    <footer className="flex items-center justify-between px-10 md:px-40 py-13 border-t bg-[#EBE1D0] border-[#EBE2E9] ">
 
         <h1 className="uppercase tracking-[0.4em] zen-antique-soft-logo text-[10px] md:text-[13px] text-[#2C2A28] opacity-60 "     >Couvrant</h1>
 
